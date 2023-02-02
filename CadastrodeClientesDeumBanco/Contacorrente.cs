837b7416fc59bd6d0e981bf1a96c7a274889da30
EdilsonBispoCEO <98287629+EdilsonBispoCEO@users.noreply.github.com>
2023-02-02 12:07:55 -0300

    Add files via upload

---
+++ CadastrodeClientesDeumBanco/Contacorrente.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CadastrodeClientesDeumBanco;
+
+
+
+internal class Contacorrente
+{
+    public string nome;
+    public int agencia;
+    public string conta;
+    public double saldo;
+    public double valorTrans;
+  
+
+   // public void Depositar(double valor)
+
+  //  {
+   //     this.saldo += valor;
+
+
+   // }
+      public bool Saque(double valor)
+     
+      {
+         
+         if (this.saldo >= valor)
+         {
+
+            this.saldo -= valor;
+            return true;
+         }
+         else
+         {
+            Console.WriteLine("saldo" + "insufisiente para realizar o saque");
+            return false;
+         }
+
+            
+
+      }
+      public bool Transferencia(double valor, Contacorrente contaDestino)
+      {
+        if (this.saldo >= valor)
+        {
+            valorTrans= valor;
+            this.saldo -= valor;
+            contaDestino.saldo += valor;
+            contaDestino.saldo -= valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Não foi possível insufisiente para realizar o saque");
+            return false;
+        }
+ 
+      }
+}
+
---
+++ CadastrodeClientesDeumBanco/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastrodeClientesDeumBanco
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+                   // Aula 01/02/23
+                Contacorrente conta1 = new Contacorrente();
+                conta1.agencia = 59;
+                conta1.conta = ("3214-x");
+                conta1.nome = "Natalia";
+                conta1.saldo = 300;
+                
+
+
+
+        // Console.WriteLine("Natalia sua conta é "+conta1.agencia+" "+ conta1.conta+ ". Seu saldo atual é "+ conta1.saldo);
+        // conta1.saldo = 550;
+        // Console.WriteLine("O saldo anterior " + conta1.saldo );
+        // conta1.Depositar(150);
+        // Console.WriteLine("O saldo atual é " + conta1.saldo);
+
+
+
+        //  Projeto 1
+             Contacorrente conta2 = new Contacorrente();
+              conta2.agencia = 59;
+              conta2.conta = "3214-x";
+              conta2.nome = "Natalia";
+
+              conta2.saldo = 550;
+
+            Console.WriteLine("   Natalia sua conta é  " +"  \n   " +   conta1.agencia + " " + "\n   "
+                + conta1.conta +  "\n" 
+              +  " Seu saldo atual é " + conta1.saldo);
+            // Console.WriteLine(" o saldo da sua conta é: " + conta2.saldo);
+
+            // if (conta2.Saque(150)) 
+            //  Console.WriteLine(" Saldo atual após saque: " + conta2.saldo);
+
+
+            Console.WriteLine(" Saldo atual da conta de transferencia: " + conta1.saldo);
+            Console.WriteLine(" Saldo atual da conta que ira receber: " + conta2.saldo);
+            if (conta1.Transferencia(10, conta2))
+                Console.WriteLine("");
+            Console.WriteLine(" Saldo atual após a transferencia: " + conta1.saldo);
+            Console.WriteLine(" Valor de transferencia: " + conta1.valorTrans);
+            Console.WriteLine(" Saldo atual após saque: " + conta2.saldo);
+
+
+        }
+    }
+}
